ac0a46e975898f349e25b07f17d38ec405d983c8
Pyrotchnst <csundheim@gmail.com>
2022-08-11 20:48:50 -0600

    added variables and functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,64 +2,71 @@
 //Global Variables
 var startButton = document.querySelector("#start");
 var h1 = document.querySelector("#h1");
+var optionElement = document.querySelector("#choices");
+var questions = [
+  {
+    title: "How many bones are there on a Skull & Crossbones flag?",
+    correctAnswer: "Three",
+    choices: ["One", "Two", "Three"],
+  },
+  {
+    title: "In nautical terms, what is the opposite of port?",
+    correctAnswer: "Starboard",
+    choices: ["Moonboard", "Starboard", "Seaboard"],
+  },
+  {
+    title: "Which sea creature has three hearts?",
+    correctAnswer: "Octopus",
+    choices: ["Minnow", "Shark", "Octopus"],
+  },
+  {
+    title: "Where would you put a baggy wrinkle on your yacht?",
+    correctAnswer: "Shroud",
+    choices: ["Shroud", "Shaull", "Skirt"],
+  },
+  {
+    title: "On a sailboat, what is a sheet?",
+    correctAnswer: "Lines",
+    choices: ["Map", "Lines", "Bedding"],
+  },
+];
+
 var currentIndex = 0;
-var timeLeft =
-  //start function
-  startButton.addEventListener("click", function () {
-    console.log("Start");
-    //clear start button and <h1>
-    startButton.setAttribute("style", "display: none");
-    h1.setAttribute("style", "display: none");
-    //Create question and ul
+var timeLeft;
 
-    var timer = setInterval(countdown, 1000);
-    function countdown() {
-      if (timeLeft === 0) {
-        alert("Time is up.");
-        clearInterval(timerId);
-        return;
-      } else {
-        timeLeft--;
-      }
-      console.log(timeLeft);
-      Element.textContent = timeLeft + " seconds remaining.";
-    }
-    // const question1 = document.createElement("ul");
-    // question1.innerHTML = "Question.....";
-    // const btn = document.createElement("button");
+function startQuiz() {
+  var currentQuestion = questions[currentIndex];
+  var titleEl = document.getElementById("questionTitle");
+  titleEl.textContent = currentQuestion.title;
+  for (var i = 0; i < currentQuestion.choices.length; i++) {
+    var option = currentQuestion.choices[i];
+    var optionButton = document.createElement("button");
+    optionButton.setAttribute("value", option);
+    optionButton.textContent = option;
+    optionElement.appendChild(optionButton);
+  }
+}
+//start function
+startButton.addEventListener("click", function () {
+  console.log("Start");
+  //clear start button and <h1>
+  startButton.setAttribute("style", "display: none");
+  h1.setAttribute("style", "display: none");
 
-    // a1.innerHTML = "answer";
-  });
+  timerCount = 60;
+  currentScore = 0;
+  // startTimer();
+  startQuiz();
+});
 
-// // Selects element by class
-// var timeEl = document.querySelector(".time");
-
-// // Selects element by id
-// var mainEl = document.getElementById("main");
-
-// var secondsLeft = 120;
-
-// function setTime() {
-//   // Sets interval in variable
-//   var timerInterval = setInterval(function () {
-//     secondsLeft--;
-//     timeEl.textContent = secondsLeft;
-
-//     if (secondsLeft === 0) {
-//       // Stops execution of action at set interval
-//       clearInterval(timerInterval);
-//       // Calls function to create and append image
-//       sendMessage();
-//     }
-//   }, 1000);
-// }
-
-// // Function to create and append colorsplosion image
-// function sendMessage() {
-//   timeEl.textContent = " ";
-//   var imgEl = document.createElement("img");
-//   imgEl.setAttribute("src", "images/image_1.jpg");
-//   mainEl.appendChild(imgEl);
-// }
-
-// setTime();
+// var timer = setInterval(countdown, 1000);
+// function countdown() {
+//   if (timeLeft === 0) {
+//     alert("Time is up.");
+//     clearInterval(timerId);
+//     return;
+//   } else {
+//     timeLeft--;
+//   }
+//   console.log(timeLeft);
+//   Element.textContent = timeLeft + " seconds remaining.";
